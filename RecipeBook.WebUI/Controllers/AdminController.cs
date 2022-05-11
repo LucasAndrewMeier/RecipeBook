a941ba906905fe8a627acffd8a2913c149c9b020
Lucas Meier <meiluca@dunwoody.edu>
2022-05-11 14:22:54 -0500

    Adding admin view

---
+++ RecipeBook.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RecipeBook.Domain.Abstract;
+
+namespace RecipeBook.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IRecipeRepo recipeRepo;
+        // GET: Admin
+        public AdminController(IRecipeRepo repo)
+        {
+            recipeRepo = repo;
+        }
+        public ViewResult Index()
+        {
+            return View(recipeRepo.Recipes);
+        }
+    }
+}
