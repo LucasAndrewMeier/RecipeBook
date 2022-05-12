0ad9f467fae036b44ca5a46a68bcf3dfec90e99f
Lucas Meier <meiluca@dunwoody.edu>
2022-05-11 20:25:03 -0500

    adjust loginViewModel

--- RecipeBook.WebUI/Models/LoginViewModel.cs
+++ RecipeBook.WebUI/Models/LoginViewModel.cs
@@ -12,9 +12,6 @@ namespace RecipeBook.WebUI.Models
         public string UserName { get; set; }
         [Required]
         public string Password { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
-        public string Email { get; set; }
+        
     }
 }
